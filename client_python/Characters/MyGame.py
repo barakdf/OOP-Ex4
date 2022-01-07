4042e947e519ec16f59a862b6be164017db243a0
barakdf <barakdf@gmail.com>
2022-01-08 00:52:53 +0200

    Revert "Merge branch 'yossi-branch' into Barak_branch"
    This reverts commit d99964bf225097cb1dd9a5b556173649109f3907, reversing
    changes made to 5ff5ec7bad9927708e1b33e2c7fe044d9fe610a8.

--- client_python/Characters/MyGame.py
+++ client_python/Characters/MyGame.py
@@ -1,15 +1,11 @@
 import json
 
-import numpy as np
-
 from client_python.Characters import Pokemon, Agent
 from client_python.Characters.Agent import agent
 from client_python.Characters.Pokemon import pokemon
 
-EPS = 0.00001
 
 numOfAgents = 0
-
 
 class MyGame:
     def __init__(self):
@@ -18,7 +14,7 @@ class MyGame:
 
     def add_pokemon(self, pokemon: Pokemon):
         self.pokemon_list.append(pokemon)
-        # self.pokemon_list.sort(key=pokemon.value, reverse=True)
+        #self.pokemon_list.sort(key=pokemon.value, reverse=True)
 
     def add_agent(self, agent: Agent):
         self.agent_list.append(agent)
@@ -27,31 +23,26 @@ class MyGame:
         global numOfAgents
         """Add Pokemon from JSON"""
         p_dic = json.loads(p_json)
+        self.pokemon_list.clear()
 
         for i in p_dic["Pokemons"]:
+
             pok = pokemon(value=i["Pokemon"]["value"], edge_type=i["Pokemon"]["type"],
                           pos=i["Pokemon"]["pos"].split(","))
 
             self.add_pokemon(pok)
-            print(pok.repr())
 
         """Add Agent from JSON"""
 
+
         agent_dic = json.loads(a_json)
+        self.agent_list.clear()
 
         for a in agent_dic["Agents"]:
-            t_agent = agent(id=a["Agent"]["id"], value=a["Agent"]["value"], src=a["Agent"]["src"],
-                            dest=a["Agent"]["dest"], speed=a["Agent"]["speed"], pos=a["Agent"]["pos"].split(","))
+            t_agent = agent(id=a["Agent"]["id"], value=a["Agent"]["value"], src=a["Agent"]["src"], dest=a["Agent"]["dest"], speed=a["Agent"]["speed"], pos=a["Agent"]["pos"].split(","))
             self.add_agent(t_agent)
-            print(t_agent.repr())
+
+
 
     def numAgents(self, info: str) -> int:
         return int(json.loads(info)["GameServer"]["agents"])
-
-    def dist(self, x: tuple, y: tuple):
-        a = np.array(x)
-        b = np.array(y)
-        return np.sqrt(np.sum((a - b) ** 2))
-
-    def contains(self, posP: tuple):
-        pass
